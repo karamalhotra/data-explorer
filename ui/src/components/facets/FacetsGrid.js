151461452e30ed0fd017c89144c60847f3096672
Kara Malhotra <karamalhotra@karamalhotra-macbookpro2.roam.corp.google.com>
2018-08-14 15:11:14 -0700

    Example of adding a plotly-react plot

--- ui/src/components/facets/FacetsGrid.js
+++ ui/src/components/facets/FacetsGrid.js
@@ -3,6 +3,7 @@ import FacetCard from "./FacetCard";
 
 import React, { Component } from "react";
 import { GridList, GridTile } from "material-ui/GridList";
+import Plot from "react-plotly.js";
 
 function FacetsGrid(props) {
   const facets = props.facets;
@@ -12,10 +13,22 @@ function FacetsGrid(props) {
       <FacetCard facet={facet} updateFacets={updateFacets} />
     </GridTile>
   ));
+
+  var layout = { width: 320, height: 240, title: "HI" };
   return (
-    <GridList className="gridList" cols={3} cellHeight="auto" padding={1}>
-      {facetsList}
-    </GridList>
+    <Plot
+      data={[
+        {
+          x: [1, 2, 3],
+          y: [2, 6, 3],
+          type: "scatter",
+          mode: "lines+points",
+          marker: { color: "red" }
+        },
+        { type: "bar", x: [1, 2, 3], y: [2, 5, 3] }
+      ]}
+      layout={layout}
+    />
   );
 }
 
